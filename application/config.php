dc5d7b52fa62fe2db12f1a7623dad9916edafc4c
vaibhavsharma2116 <coderaftindia@gmail.com>
2025-10-17 03:23:50 +0530

    update

---
+++ application/config.php
@@ -0,0 +1,7 @@
+<?php
+$db_host	    = 'localhost';
+$db_user        = 'software_accountingdbuser';
+$db_password	= 'kMrWS}zRb(8w';
+$db_name	    = 'software_accountingdb';
+define('APP_URL', 'https://accounting.softwarestore.biz');
+$_app_stage = 'Live'; // You can set this variable Live to Dev to enable ibilling Debug
